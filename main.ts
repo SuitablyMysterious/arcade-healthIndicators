887433990f31efb391ab6d233a9dbc8743c231ea
SuitablyMysterious <fly2be@outlook.com>
2025-02-05 17:19:11 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,18 @@
-let healthIndicatorSprites: number[] = []
-let healthIndicatorSprite: Sprite = null
-function makeHealthIndicator () {
-    healthIndicatorSprites = []
-    healthIndicatorSprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+namespace SpriteKind {
+    export const healthIndicators = SpriteKind.create()
 }
+function updateHealthIndicator () {
+	
+}
+function makeHealthIndicator (player2: Sprite, hex2: string, offsetX: number, offsetY: number) {
+    healthIndicatorSprite = textsprite.create("")
+    healthIndicatorSprite.setFlag(SpriteFlag.AutoDestroy, false)
+    sprites.setDataSprite(healthIndicatorSprite, "player", player2)
+    sprites.setDataNumber(healthIndicatorSprite, "offsetX", offsetX)
+    sprites.setDataNumber(healthIndicatorSprite, "offsety", offsetY)
+    healthIndicatorSprite.setKind(SpriteKind.healthIndicators)
+    healthIndicatorSprites.push(healthIndicatorSprite)
+}
+let healthIndicatorSprite: TextSprite = null
+let healthIndicatorSprites: TextSprite[] = []
+healthIndicatorSprites = []
